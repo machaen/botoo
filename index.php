cfdc05266634e781864c9ab8805773377cf6a424
machaen <julio.machaen@gmail.com>
2013-09-02 21:44:42 -0500

    Subiendo bootstrap

---
+++ index.php
@@ -0,0 +1,30 @@
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+    <div class=" show container">
+    <div class="row-fluid">
+    <form class="span10 text-center">
+    <h3>Iniciar sesion</h3>
+    <label>Usuario</label>
+    <input type="text" class="text-success"/><br>
+    <label>Clave</label>
+    <input type="password"/><br>
+    Recordar Clave
+    <input type="checkbox" class="checkbox"><br>
+    <a class="btn btn-primary"> Iniciar sesion</a> <a class="btn">Cancelar</a>
+    </form>
+    <div class="span2">
+    <a class="text-center text-info">Olvide mi password</a>
+    </div>
+    </div>
+    </div>
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+    </html>
